0833095a01b7e962a5a028e52fa93600fc5ac9f0
KhangVjp <94008051+KhangVjp@users.noreply.github.com>
2023-11-07 11:36:19 +0700

    Commit Dang nhap Dang ky QuanLySanPham GioHang

---
+++ app/src/main/java/com/example/ban_dt_lt/Quan_Tri_Vien/QuanLySanPham.java
@@ -0,0 +1,170 @@
+package com.example.ban_dt_lt.Quan_Tri_Vien;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.ban_dt_lt.R;
+import com.example.ban_dt_lt.Xu_Ly_Giao_Dien.DrawerBaseActivity;
+import com.example.ban_dt_lt.databinding.ActivityQuanLySanPhamBinding;
+
+public class QuanLySanPham extends DrawerBaseActivity {
+    ActivityQuanLySanPhamBinding activityQuanLySanPhamBinding;
+    ImageView iv_anh_san_phamC;
+    Button button_chon_anh_san_phamC, button_luu_san_phamC;
+    EditText tv_ten_san_phamC,tv_gia_san_phamC,tv_mo_ta_san_phamC;
+    ActivityResultLauncher<Intent> imagePickerLauncher;
+    Spinner spinner_pathC;
+    Uri selectedImageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityQuanLySanPhamBinding = ActivityQuanLySanPhamBinding.inflate(getLayoutInflater());
+        setContentView(activityQuanLySanPhamBinding.getRoot());
+        allocateActivityTitle("Them san pham");
+        //
+        iv_anh_san_phamC =findViewById(R.id.iv_anh_san_pham);
+        button_chon_anh_san_phamC = findViewById(R.id.button_chon_anh_san_pham);
+        button_luu_san_phamC = findViewById(R.id.button_luu_san_pham);
+        tv_gia_san_phamC = findViewById(R.id. tv_gia_san_pham);
+        tv_ten_san_phamC = findViewById(R.id.tv_ten_san_pham);
+        tv_mo_ta_san_phamC = findViewById(R.id.tv_mo_ta_san_pham);
+        // spinner
+        spinner_pathC = findViewById(R.id.spinner_path);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.paths_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_pathC.setAdapter(adapter);
+        //
+        button_chon_anh_san_phamC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                imagePickerLauncher.launch(intent);
+            }
+        });
+        imagePickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                Intent intent = result.getData();
+                selectedImageUri = intent.getData();
+                // Hiển thị hình ảnh đã chọn trong ImageView
+                iv_anh_san_phamC.setImageURI(selectedImageUri);
+            }
+        });
+        button_luu_san_phamC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (selectedImageUri != null) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(QuanLySanPham.this);
+                    builder.setTitle("Xác nhận");
+                    builder.setMessage("Bạn có chắc chắn muốn tải lên ảnh?");
+                    builder.setPositiveButton("Đồng ý", (dialog, which) -> {
+
+                        // Tạo một tham chiếu đến Firebase Storage
+                        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+                        // Tạo một tham chiếu con với tên duy nhất cho hình ảnh
+                        String imageName = "image_" + System.currentTimeMillis() + ".jpg";
+                        StorageReference imageRef = storageRef.child("Hinh_Anh/" + imageName);
+                        // Tải lên hình ảnh lên Firebase Storage
+                        imageRef.putFile(selectedImageUri)
+                                .addOnSuccessListener(taskSnapshot -> {
+                                    // Lấy URL tải xuống của hình ảnh
+                                    imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+                                        //
+                                        String imageURL = uri.toString();
+                                        // Lưu URL hình ảnh vào Firebase Realtime Database
+                                        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference();
+                                        DatabaseReference pathRef;
+                                        String path = spinner_pathC.getSelectedItem().toString();
+                                        String tenSP= tv_ten_san_phamC.getText().toString();
+                                        String giaSP = tv_gia_san_phamC.getText().toString();
+                                        String motaSP = tv_mo_ta_san_phamC.getText().toString();
+                                        if (path.equals("Lap Top")) {
+                                            pathRef = databaseRef.child("Lap_Top");
+                                            pathRef.child(tenSP).addListenerForSingleValueEvent(new ValueEventListener() {
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                    get_set_san_pham get_set_san_pham = new get_set_san_pham(tenSP,motaSP,giaSP,imageURL,"");
+                                                    pathRef.child(tenSP).setValue(get_set_san_pham);
+                                                    Toast.makeText(QuanLySanPham.this, "Tải ảnh thành công", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(QuanLySanPham.this, Laptop.class);
+                                                    startActivity(intent);
+                                                    finishAffinity();
+                                                }
+
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                                }
+                                            });
+                                        } else if (path.equals("Điện Thoại")) {
+                                            pathRef = databaseRef.child("Dien_Thoai");
+                                            pathRef.child(tenSP).addListenerForSingleValueEvent(new ValueEventListener() {
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                    get_set_san_pham get_set_san_pham = new get_set_san_pham(tenSP,motaSP,giaSP,imageURL,"");
+                                                    pathRef.child(tenSP).setValue(get_set_san_pham);
+                                                    Toast.makeText(QuanLySanPham.this, "Tải ảnh thành công", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(QuanLySanPham.this, Laptop.class);
+                                                    startActivity(intent);
+                                                    finishAffinity();
+                                                }
+
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                                }
+                                            });
+                                        } else if (path.equals("Loa")) {
+                                            pathRef = databaseRef.child("Loa");
+                                            pathRef.child(tenSP).addListenerForSingleValueEvent(new ValueEventListener() {
+                                                @Override
+                                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                    get_set_san_pham get_set_san_pham = new get_set_san_pham(tenSP,motaSP,giaSP,imageURL,"");
+                                                    pathRef.child(tenSP).setValue(get_set_san_pham);
+                                                    Toast.makeText(QuanLySanPham.this, "Tải ảnh thành công", Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(QuanLySanPham.this, Laptop.class);
+                                                    startActivity(intent);
+                                                    finishAffinity();
+                                                }
+
+                                                @Override
+                                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                                }
+                                            });
+                                        }
+                                        //
+                                    });
+                                })
+                                .addOnFailureListener(e -> {
+                                    Toast.makeText(QuanLySanPham.this, "Tải ảnh không thành công", Toast.LENGTH_SHORT).show();
+                                });
+                    });
+                    builder.setNegativeButton("Hủy", (dialog, which) -> {
+                        Toast.makeText(QuanLySanPham.this, "Hủy thành công", Toast.LENGTH_SHORT).show();
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    Toast.makeText(QuanLySanPham.this, "Vui lòng chọn hình ảnh trước khi tải lên", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/ban_dt_lt/Xu_Ly_Danh_Sach/get_set_nguoi_dung.java
@@ -0,0 +1,57 @@
+package com.example.ban_dt_lt.Xu_Ly_Danh_Sach;
+
+public class get_set_nguoi_dung {
+    String ten;
+    String so_dien_thoai;
+    String mat_khau;
+    String dia_chi;
+    String gio_hang;
+
+    public get_set_nguoi_dung(String ten, String so_dien_thoai, String mat_khau,String dia_chi,String gio_hang) {
+        this.ten = ten;
+        this.so_dien_thoai = so_dien_thoai;
+        this.mat_khau = mat_khau;
+        this.dia_chi = dia_chi;
+        this.gio_hang = gio_hang;
+    }
+
+    public String getGio_hang() {
+        return gio_hang;
+    }
+
+    public void setGio_hang(String gio_hang) {
+        this.gio_hang = gio_hang;
+    }
+
+    public String getDia_chi() {
+        return dia_chi;
+    }
+
+    public void setDia_chi(String dia_chi) {
+        this.dia_chi = dia_chi;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getSo_dien_thoai() {
+        return so_dien_thoai;
+    }
+
+    public void setSo_dien_thoai(String so_dien_thoai) {
+        this.so_dien_thoai = so_dien_thoai;
+    }
+
+    public String getMat_khau() {
+        return mat_khau;
+    }
+
+    public void setMat_khau(String mat_khau) {
+        this.mat_khau = mat_khau;
+    }
+}
